192294694ce082661fd13f1c01e69dd6974e17f1
Peter Schulze <petschul@cisco.com>
2019-02-27 09:21:17 -0700

    allow file upload

--- app/controllers/yaml_samples_controller.rb
+++ app/controllers/yaml_samples_controller.rb
@@ -1,12 +1,51 @@
+require 'zip'
+
 class YamlSamplesController < ApplicationController
   def create
+    if yaml_sample_params[:text]
+      create_from_text
+    elsif yaml_sample_params[:file]
+      file_type = yaml_sample_params[:file].content_type
+      logger.info file_type
+      if file_type == "application/zip"
+        create_from_zip_file
+      elsif
+        create_from_yaml_file
+      end
+    end
+  end
+
+  def create_from_text
     @yaml_sample = YamlSample.new(yaml_sample_params[:text])
     render @yaml_sample
+  end
+
+  def create_from_yaml_file
+    yaml_text = yaml_sample_params[:file].read
+    @yaml_sample = YamlSample.new(yaml_text)
+    render @yaml_sample
+  end
+
+  def create_from_zip_file
+    Zip::File.open(yaml_sample_params[:file].open) do |zip_file|
+      @yaml_samples = []
+      zip_file.each do |yaml_file|
+        # Move to next file if it's a directory, not a .yml file, or one of the funky osx files
+        if yaml_file.ftype == :directory ||
+           !yaml_file.name.match?(/(.yml)\z/) ||
+           yaml_file.name.match?(/\A(__MACOSX)/)
+          next
+        end
+        yaml_text = yaml_file.get_input_stream.read
+        @yaml_samples << YamlSample.new(yaml_text)
+      end
+    end
+    render "yaml_samples"
   end
 
   private
 
   def yaml_sample_params
-    params.require(:yaml_sample).permit(:text)
+    params.require(:yaml_sample).permit(:text, :file)
   end
 end
--- test/models/yaml_sample_test.rb
+++ test/models/yaml_sample_test.rb
@@ -1,26 +1,29 @@
-require 'test_helper'
+require "test_helper"
 
 class YamlSampleTest < ActiveSupport::TestCase
-  def setup
+  setup do
     @valid_yaml_sample =
-      YamlSample.new(file_fixture('valid.yml.example').read)
+      YamlSample.new(file_fixture("valid.yml.example").read)
     @invalid_yaml_sample =
-      YamlSample.new(file_fixture('invalid.yml.example').read)
+      YamlSample.new(file_fixture("invalid.yml.example").read)
   end
 
-  def test_errors_with_valid_yaml
+  # -----=====[ errors() ]=====-----
+
+  test "should have no errors with valid YAML" do
     assert_nil(@valid_yaml_sample.errors)
   end
 
-  def test_errors_with_invalid_yaml
+  test "should have an error with invalid yaml" do
     assert_includes(@invalid_yaml_sample.errors, :message)
   end
 
-  def test_locale_with_valid_yaml
+  # -----=====[ locale() ]=====-----
+  test "should give the locale of a valid yaml file" do
     assert_equal(@valid_yaml_sample.locale, :en)
   end
 
-  def test_locale_with_invalid_yaml
+  test "should give no locale for an invalid yaml file" do
     assert_nil(@invalid_yaml_sample.locale)
   end
 end
