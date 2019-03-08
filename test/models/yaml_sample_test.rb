64d8ab3a2889353b136df2c70d1a9260e34071fe
Peter Schulze <petschul@cisco.com>
2019-03-08 09:58:53 -0700

    yaml with symbols test

--- test/models/yaml_sample_test.rb
+++ test/models/yaml_sample_test.rb
@@ -6,6 +6,8 @@ class YamlSampleTest < ActiveSupport::TestCase
       YamlSample.new(file_fixture("valid.yml.example").read)
     @invalid_yaml_sample =
       YamlSample.new(file_fixture("invalid.yml.example").read)
+    @valid_yaml_sample_with_symbols =
+      YamlSample.new(file_fixture("valid_with_symbols.yml.example").read)
   end
 
   # -----=====[ errors() ]=====-----
@@ -14,16 +16,20 @@ class YamlSampleTest < ActiveSupport::TestCase
     assert_nil(@valid_yaml_sample.errors)
   end
 
-  test "should have an error with invalid yaml" do
+  test "should have an error with invalid YAML" do
     assert_includes(@invalid_yaml_sample.errors, :message)
   end
 
+  test "should not show errors on a valid YAML file containing symbols" do
+    assert_nil(@valid_yaml_sample_with_symbols.errors)
+  end
+
   # -----=====[ locale() ]=====-----
-  test "should give the locale of a valid yaml file" do
+  test "should give the locale of a valid YAML file" do
     assert_equal(@valid_yaml_sample.locale, :en)
   end
 
-  test "should give no locale for an invalid yaml file" do
+  test "should give no locale for an invalid YAML file" do
     assert_nil(@invalid_yaml_sample.locale)
   end
 end
